0e005bcc1381524af3de18110f9e7cd38dfeee78
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-01-13 06:05:03 -0800

    Add babel config for Jests
    - Jest cannot test without converting ESM to CJS

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  rootDir: './',
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+  verbose: true,
+  bail: true,
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/pkg/', '/.history/'],
+};
